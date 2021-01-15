867434413e169f3b40386f58eb63eb2a6d30d080
Thomas Griffiths <58866258+TopicalTom@users.noreply.github.com>
2021-01-15 14:03:41 -0500

    Built out base Hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Hero />
+        </div>
+    );
 }
 
 export default App;
